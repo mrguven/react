b502611f43b8851ad270967360b40e76837285a6
Rauf Guven <mrguven@gmail.com>
2023-06-19 17:42:18 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Capture.png" className="App-logo" alt="logo" />
+        <img src="logo512.png" className="App-logo" alt="logo" />
 
         <a
           className="App-link"
