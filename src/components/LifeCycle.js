102be245000100d760d2d3dc7c6710a162cf2875
Rauf Guven <mrguven@gmail.com>
2023-06-22 13:32:16 +0200

    update

--- src/components/LifeCycle.js
+++ src/components/LifeCycle.js
@@ -1,12 +1,16 @@
 import { Component } from "react";
 import axios from "axios";
 
+
 export default class LifeCycle extends Component {
 
     constructor(props) {
         super(props)
         this.state={
-            user:null
+            user:null,
+            users:[],
+            isLoading:true,
+            age:20
         }
     }
 
@@ -21,7 +25,13 @@ export default class LifeCycle extends Component {
 
 componentDidMount(){
     axios.get('https://jsonplaceholder.typicode.com/users')
-    .then((data)=> { console.log(data.data[0])})
+    .then((data)=> { console.log(data.data);
+        setTimeout(() => {
+            this.setState({users:data.data ,isLoading:false,age:25})
+            
+        }, 3000);
+        
+    })
     .catch((err)=> console.log(err))
 }
 
@@ -36,7 +46,36 @@ console.log('this is will mount');
     render() {
         return (
              <div>
+
+
+
+
+
                 <h1>{this.state.user}</h1>
+                <h1>{this.state.age}</h1>
+                {
+                    this.state.isLoading 
+                    ? 
+
+                    <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                  </div> 
+                                :
+                    this.state.users.map((person)=>{
+                        return (
+                            <div>
+                        <ul>
+                            <li>
+                        <h1>{person.name}</h1> 
+                        <h1>{person.phone}</h1>
+                        <h1>{person.email}</h1>
+                        </li>
+                        </ul>
+                        </div>
+                        )
+                    })
+
+}
              </div>
         );
     }
